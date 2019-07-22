dffc9209ef684647217ce9858fe151ebbbbc0f7d
Abin <porag214@gmail.com>
2019-07-22 23:24:20 +0600

    test commit

---
+++ .history/src/app/app.module_20190722221906.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    AppRoutingModule
+  ],
+  declarations: []
+})
+export class AppModule { }
